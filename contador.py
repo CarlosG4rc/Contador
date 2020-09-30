ec5c6b9c8a2f65df91bd48afa05599af5617f35c
Carlos García <67180099+CarlosG4rc@users.noreply.github.com>
2020-09-30 17:33:01 -0500

    Add files via upload

---
+++ contador.py
@@ -0,0 +1,35 @@
+from io import open
+import sys
+
+fichero = open("contador.txt", "a+")
+fichero.seek(0)
+contenido = fichero.readline()
+
+if len(contenido) == 0:
+    contenido = "0"
+    fichero.write(contenido)
+
+fichero.close()
+
+#Vamos a intentar transformar el texto a un número
+
+try:
+    contador = int(contenido)
+
+    #En fucnion de lo que el usuario quiera...
+    if len(sys.argv) == 2:
+        if sys.argv[1] == "inc":
+            contador += 1
+        elif sys.argv[1] == "dec":
+            contador -= 1
+    print(contador)
+
+
+    #finalmente vovlemos a escribir los cambios en el fichero
+    fichero = open("contador.txt", "w")
+    fichero.write(str(contador))
+    fichero.close()
+
+except:
+    print("Error: Fichero erroneo.")
+
